d81d3b0dc2214519e042a611960a4a1c067f64f6
Thiago Ferreira <thiaago.dev@gmail.com>
2021-07-07 19:03:22 -0300

    Validando as datas

--- passagens/validation.py
+++ passagens/validation.py
@@ -1,7 +1,19 @@
 def origem_destino_iguais(origem, destino, lista_de_erros):
-     if origem == destino:
-            lista_de_erros['destino'] = 'Origem e destino não podem ser iguais!'
+    """  Verifica se a origem e o destino são iguais """
+    if origem == destino:
+        lista_de_erros['destino'] = 'Origem e destino não podem ser iguais!'
 
 def campo_tem_algum_numero(valor_campo, nome_campo, lista_de_erros):
-     if any(char.isdigit() for char in valor_campo ):
-            lista_de_erros[nome_campo] = 'Não inclua números neste campo!'
+    """ Verifica se um determinado campo fornecido possua números  """
+    if any(char.isdigit() for char in valor_campo ):
+        lista_de_erros[nome_campo] = 'Não inclua números neste campo!'
+
+def data_ida_maior_que_data_volta(data_ida, data_volta, lista_de_erros):
+    """  Verifica se data de ida é maior que a data de volta  """
+    if data_ida > data_volta:
+        lista_de_erros['data_volta'] = 'A data de volta não pode ser menor que a data de ida!'
+
+def data_ida_menor_que_data_de_hoje(data_ida, data_pesquisa, lista_de_erros):
+    """ Verifica se a data de ida é menor que a data de hoje """
+    if data_ida < data_pesquisa:
+        lista_de_erros['data_ida'] = 'A data de ida não pode ser menor que a data de hoje!'
